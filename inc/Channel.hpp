7081b47fa65e9e7529c0eda5500b135273ec0340
Arthur Li <ali@desktop.home>
2022-10-27 17:43:43 +0200

    Implémentation de Channel (non testé encore)

--- inc/Channel.hpp
+++ inc/Channel.hpp
@@ -6,12 +6,16 @@
 /*   By: ali <ali@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 13:32:51 by ali               #+#    #+#             */
-/*   Updated: 2022/10/27 13:43:35 by ali              ###   ########.fr       */
+/*   Updated: 2022/10/27 17:41:12 by ali              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CHANNEL_HPP
 # define CHANNEL_HPP
+
+# include <map>
+# include <vector>
+# include <iostream>
 
 # include "User.hpp"
 
@@ -19,7 +23,52 @@ class User;
 
 class Channel
 {
+	private:
+		std::string	name;
+		std::string	topic;
+		std::map<std::string, bool>	flags;
+		std::vector<std::string> inviteList;
+		std::vector<std::string> banList;
+		std::map< std::map<std::string, User*> users;
+		std::map<std::string, std::map<std::string, bool> > userModes;
+		// map to check for each user if voiced or if chanOp
+		std::string	key;
+		int	userLimit;
 
+	public:
+		Channel(const std::string& aName);
+		~Channel();
+
+		const std::string&	getName() const;
+		const std::string&	getTopic() const;
+		int	getKey() const;
+		int	getUserLimit() const;
+		const std::vector<User*>	getUserList() const;
+		const std::vector<std::string> getNickList() const;
+
+		bool	isInvited(const std::string& nick) const;
+		bool	isBanned(const std::string& nick) const;
+		bool	isChanOp(const std::string&	nick) const;
+		bool	isVoiced(const std::string& nick) const;
+
+		bool	isSecret() const;
+		bool	isModerated() const;
+		bool	isInviteOnly() const;
+		bool	isAnonymous() const;
+
+		banUser(const std::string& nick);
+		inviteUser(const std::string& nick);
+
+		kickUser(const std::string &nick);
+		addUser(User* user);
+
+		setName(const std::string& aName);
+		setTopic(const std::string& aTopic);
+		setKey(int aKey);
+		setUserLimit(const int& aUserLimit);
+		setChanFlag(const std::string& flag, bool b);
+		setUserOp(const std::string& nick, bool b);
+		setUserVoiced(const std::string& nick, bool b);
 };
 
 #endif
---
+++ srcs/Channel.cpp
@@ -0,0 +1,191 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Channel.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ali <ali@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/27 16:39:02 by ali               #+#    #+#             */
+/*   Updated: 2022/10/27 17:40:58 by ali              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/Channel.hpp"
+
+Channel::Channel(const std::string& aName) : name(aName) 
+{
+	topic = "";
+	flags["secret"] = false;
+	flags["moderated"] = false;
+	flags["inviteOnly"] = false;
+	flags["anonymous"] = false;
+	
+	key = "";
+	userLimit = 500;
+}
+
+~Channel::~Channel()
+{
+	flags.clear();
+	inviteList.clear();
+	banList.clear();
+	users.clear();
+}
+
+const std::string&	Channel::getName() const
+{
+	return name;
+}
+
+const std::string&	Channel::getTopic() const
+{
+	return topic;
+}
+
+int	Channel::getKey() const
+{
+	return key;
+}
+
+int	Channel::getUserLimit() const
+{
+	return userLimit;
+}
+
+const std::string&	Channel::getUserList() const
+{
+	vector<User*> userList;
+	for (std::map<std::string, User*>::iterator it = users.begin();
+			it != users.end(); it++)
+		userList.push_back(it->second);
+	return userList;
+}
+
+const std::string&	Channel::getNickList() const
+{
+	vector<User*> nickList;
+	for (std::map<std::string, User*>::iterator it = users.begin();
+			it != users.end(); it++)
+		nickList.push_back(it->first);
+	return nickList;
+}
+
+bool	Channel::isInvited(const std::string& nick) const
+{
+	if (std::find(inviteList.begin(), inviteList.end(), nick) != inviteList.end())
+		return true;
+	return false
+}
+
+bool	Channel::isBanned(const std::string& nick) const
+{
+	if (std::find(banList.begin(), banList.end(), nick) != banList.end())
+		return true;
+	return false
+}
+
+bool	Channel::isChanOp(const std::string& nick) const
+{
+	if (userModes[nick]["operator"] == true)
+		return true;
+	return false
+}
+
+bool	Channel::isVoiced(const std::string& nick) const
+{
+	if (userModes[nick]["voiced"] == true)
+		return true;
+	return false
+}
+
+bool	Channel::isSecret() const
+{
+	if (flags["secret"] == true)
+		return true;
+	return false
+}
+
+bool	Channel::isModerated() const
+{
+	if (flags["moderated"] == true)
+		return true;
+	return false
+}
+
+bool	Channel::isInviteOnly() const
+{
+	if (flags["inviteOnly"] == true)
+		return true;
+	return false
+}
+
+bool	Channel::isAnonymous() const
+{
+	if (flags["anonymous"] == true)
+		return true;
+	return false
+}
+
+void	Channel::banUser(const std::string& nick)
+{
+	banList.push_back(nick);
+	users.erase(nick);
+	userModes.erase(nick)
+}
+
+void	Channel::inviteUser(const std::string& nick)
+{
+	inviteList.push_back(nick);
+}
+
+void	Channel::kickUser(const std::string& nick)
+{
+	users.erase(nick);
+	userModes.erase(nick)
+}
+
+void	Channel::addUser(User* user)
+{
+	std::string nick = user->get_nick();
+	users[nick] = user;
+	if userModes.empty()
+		userModes[nick]["operator"] = true;
+	else
+		userModes[nick]["operator"] = false;
+	userModes[nick]["voiced"] = false;
+}
+
+void	Channel::setName(const std::string& aName)
+{
+	name = aName;
+}
+
+void	Channel::setTopic(const std::string& aTopic)
+{
+	topic = aTopic;
+}
+
+void	Channel::setKey(int aKey)
+{
+	key = aKey;
+}
+
+void	Channel::setUserLimit(int aUserLimit)
+{
+	userLimit = aUserLimit;
+}
+
+void	Channel::setChanFlag(const std::string& flag, bool b)
+{
+	flags[flag] = b;
+}
+
+void	Channel::setUserOp(const std::string& nick, bool b)
+{
+	userModes[nick] = b;
+}
+
+void	Channel::setUserVoiced(const std::string& nick, bool b)
+{
+	userModes[nick] = b;
+}
