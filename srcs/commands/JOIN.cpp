c83c87e7f8f28dd66f055f5b6101074255ceeb01
amontaut <alice.montaut@gmail.com>
2022-10-31 15:15:47 +0100

    push for 42

--- srcs/commands/JOIN.cpp
+++ srcs/commands/JOIN.cpp
@@ -3,7 +3,18 @@
 
 void JOIN(User *user)
 {
+    // Pattern : ( <channel> *( "," <channel> ) [ <key> *( "," <key> ) ] )
+    //    / "0"
+    // #chan
+    // #chan key
+    // #chan, #chan
+    // #chan, #chan key, key
+    // #chan, #chan key
+
+    //     +pareil avec &instead of #
+
     std::cout << "Hello JOIN" << std::endl;
+
     // check debut is # ou & quand cest pas une key
     // check needmore param s'il faut une key (ERR_NEEDMOREPARAMS)
     // Key correct (ERR_BADCHANNELKEY)
@@ -12,12 +23,12 @@ void JOIN(User *user)
     // Channel names should only include lower and uppercase letters, numbers and the following punctuation _ - = @ , . ; ERR_NOSUCHCHANNEL
     // Cannot join chan is banned (ERR_BANNEDFROMCHAN)
     // ERR_CHANNELISFULL (500)
-    //joined too many chans : ERR_TOOMANYCHANNELS
+    // joined too many chans : ERR_TOOMANYCHANNELS
     // ERR_TOOMANYTARGETS : Returned to a client which is attempting to JOIN a safe
     //        channel using the shortname when there are more than one
     //        such channel. --> Necessaire ?
-    //ERR_BADCHANMASK --> Necessaire ?
-    //ERR_UNAVAILRESOURCE ? 
+    // ERR_BADCHANMASK --> Necessaire ?
+    // ERR_UNAVAILRESOURCE ?
     // JOIN 0 = leave all channel grace via PART
     int j = 0;
     while (j <= user->param_list.size())
@@ -25,35 +36,46 @@ void JOIN(User *user)
         std::cout << "params JOINS are: " << user->param_list[j] << std::endl;
         j++;
     }
-
-    if (user->param_list[0] == "0")
-    {
-        // Part(all_channels);
-    }
-    else
-    {
-        int i = 0;
-        while (i < user->param_list.size())
-        {
-            user->param_list[i].erase(0, 1);
-            std::vector<std::string> channel_list = user->get_server()->get_channels_list();
-            if (std::find(channel_list.begin(), channel_list.end(), user->param_list[i]) == channel_list.end())
-            {
-                user->get_server()->add_channel(user->param_list[i]);
-                // add operator mode for this chan for this user
-            }
-            else
-            {
-                // joindre le chan deja cree
-                // RPL_TOPIC
-                // RPL_NAMREPLY (353)
-                // RPL_ENDOFNAMES (366).
-            }
-            i++;
-        }
-    }
+    int i = 0;
+    if (user->param_list[0][0] == '#' || user->param_list[0][0] == '&')
+        user->param_list[0].erase(0, 1);
     std::vector<std::string> channel_list = user->get_server()->get_channels_list();
+    if (std::find(channel_list.begin(), channel_list.end(), user->param_list[i]) == channel_list.end())
+    {
+        // creation du chan
+        user->get_server()->add_channel(user->param_list[i]);
+        // add operator mode for this chan for this user
+    }
 
+    // if (user->param_list[0] == "0")
+    // {
+    //     // Part(all_channels);
+    // }
+    // else
+    // {
+    //     int i = 0;
+    //     while (i < user->param_list.size())
+    //     {
+    //         user->param_list[i].erase(0, 1);
+    //         std::vector<std::string> channel_list = user->get_server()->get_channels_list();
+    //         if (std::find(channel_list.begin(), channel_list.end(), user->param_list[i]) == channel_list.end())
+    //         {
+    //             //creation du chan
+    //             user->get_server()->add_channel(user->param_list[i]);
+    //             // add operator mode for this chan for this user
+    //         }
+    //         else
+    //         {
+    //             // joindre le chan deja cree
+    //             // RPL_TOPIC
+    //             // RPL_NAMREPLY (353)
+    //             // RPL_ENDOFNAMES (366).
+    //         }
+    //         i++;
+    //     }
+    // }
+
+    std::vector<std::string> channel_list = user->get_server()->get_channels_list();
     std::cout << "Now list chan is" << std::endl;
     for (std::vector<std::string>::iterator it = channel_list.begin(); it != channel_list.end(); ++it)
         std::cout << " " << *it;
--- srcs/commands/NICK.cpp
+++ srcs/commands/NICK.cpp
@@ -2,6 +2,7 @@
 #include "../../inc/colors.hpp"
 #include "../../inc/User.hpp"
 #include "../../inc/Server.hpp"
+#include "../../inc/replies.hpp"
 #include <iostream>
 #include <map>
 #include <string>
@@ -32,7 +33,9 @@ int check_forbiden_char(std::string nick)
     {
         int i = 0;
         if (nick.length() != 1 && nick[i] == ':') // tbc
+        {
             i++;
+        }
         if (nick.length() == 1 && isalpha(nick[i]) == 0 && specialchar(nick[i]) == 0)
             return (-1);
         while (i < nick.length() - 1)
@@ -41,6 +44,7 @@ int check_forbiden_char(std::string nick)
                 return (-1);
             i++;
         }
+        std::cout << "CHAR IS " << nick[i] << std::endl;
         if (isalnum(nick[i]) == 0 && specialchar(nick[i]) == 0)
             return (-1);
         return (0);
@@ -50,32 +54,27 @@ int check_forbiden_char(std::string nick)
 
 void NICK(User *user)
 {
-    std::string old_nick = user->get_nick();
-    std::vector<std::string> nick_list = user->get_server()->get_nick_list();
     if (user->param_list[0][0] == ':') // tbc
         user->param_list[0].erase(0, 1);
-    user->param_list[0] = user->param_list[0].substr(0, 9);
-    if (user->param_list[0] == "")                                       // quand on passe NICK dans irssi sans param ca fait rien a part redire le nick
+    std::string nickname = user->param_list[0].substr(0, 9);
+    // user->param_list[0] = user->param_list[0].substr(0, 9);
+    if (user->param_list.size() == 0)                                    // quand on passe NICK dans irssi sans param ca fait rien a part redire le nick
         std::cout << RED "Error 431 ERR_NONICKNAMEGIVEN" E << std::endl; // ajouter reply
-    if (check_forbiden_char(user->param_list[0]) == -1)
+
+    if (check_forbiden_char(nickname) == -1)
         std::cout << RED "Error 432 ERR_ERRONEUSNICKNAME" E << std::endl; // ajouter reply
-    if (user->param_list[0] == old_nick)
+    std::map<int, User *> users = user->get_server()->get_user_list();
+    for (std::map<int, User *>::iterator it = users.begin(); it != users.end(); it++)
     {
-        std::cout << YELLOW "This is already your NICK" E << std::endl; // utile ?
-    }
-    else if (std::count(nick_list.begin(), nick_list.end(), user->param_list[0]))
-    {
-        std::cout << RED "Error 433 ERR_NICKNAMEINUSE" E << std::endl; // ajouter reply
-    }
-    else
-    {
-        user->set_nick(user->param_list[0]);
-        user->get_server()->add_nick(user->param_list[0]);
-        user->get_server()->remove_nick(old_nick);
+        if (nickname == it->second->get_nick())
+            std::cout << RED "Error 433 ERR_NICKNAMEINUSE" E << std::endl; // ajouter reply
     }
 
+    user->set_nick(nickname);
+
     // 437    ERR_UNAVAILRESOURCE ?
+    // 433    ERR_NICKNAMEINUSE : user is not a valid type
     // 484    ERR_RESTRICTED ?? : Sent by the server to a user upon connection to indicate
     //    the restricted nature of the connection (user mode "+r").
-    std::cout << BBLUE "Your nick is: " << user->get_nick() << E << std::endl;
+    // std::cout << "VAR NICK IS : " << user->get_nick() << std::endl;
 }
