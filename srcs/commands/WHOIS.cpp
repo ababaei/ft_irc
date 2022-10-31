3cbac05e9fc32db051a9f9da5135a1afb4bb2bf2
ababaei <kzmmaz@gmail.com>
2022-10-31 16:43:15 +0100

    start of WHOIS

--- srcs/commands/WHOIS.cpp
+++ srcs/commands/WHOIS.cpp
@@ -1,7 +1,11 @@
 #include "../../inc/command.hpp"
 #include "../../inc/Server.hpp"
+#include "../../inc/replies.hpp"
 
-void    WHOIS(User *User)
+void    WHOIS(User *user)
 {
+    std::vector<std::string> params = user->param_list;
+    if (params.size() == 0)
+        return (user->get_server()->to_send(ERR_NONICKNAMEGIVEN(getArgs(0), user->get_nick())));
     std::cout << "Hello WHOIS" << std::endl;
 }
