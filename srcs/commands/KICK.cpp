714640e91c36b86250f80184228308c7d231a841
Arthur Li <ali@desktop.home>
2022-11-01 15:15:36 +0100

    KICK et PART compilent

--- srcs/commands/KICK.cpp
+++ srcs/commands/KICK.cpp
@@ -6,18 +6,39 @@
 /*   By: ali <ali@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 11:42:08 by ali               #+#    #+#             */
-/*   Updated: 2022/11/01 11:50:21 by ali              ###   ########.fr       */
+/*   Updated: 2022/11/01 15:10:01 by ali              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../inc/commands.hpp"
+#include "../../inc/command.hpp"
 
 void	KICK(User* user)
 {
 	std::vector<std::string> params = user->param_list;
 
 	if (params.size() < 2)
-		return user->get_server()->to_send(ERR_NEEDMOREPARAMS(getArgs(1, "PART"),
+		return user->get_server()->to_send(ERR_NEEDMOREPARAMS(getArgs(1, "KICK"),
 					user->get_nick()), user->get_fd());
 
+	Channel* chan = user->get_server()->get_channel(params[0]);
+	if (chan == NULL)
+		return user->get_server()->to_send(ERR_NOSUCHCHANNEL(getArgs(1, params[0]),
+					user->get_nick()), user->get_fd());
+
+	if (chan->isHere(user->get_nick()) == false)
+		return user->get_server()->to_send(ERR_NOTONCHANNEL(getArgs(1, params[0]),
+				user->get_nick()), user->get_fd());
+
+	if (chan->isChanOp(user->get_nick()) == false)
+		return user->get_server()->to_send(ERR_CHANOPRIVSNEEDED(getArgs(1, params[0]),
+					user->get_nick()), user->get_fd());
+
+	if (chan->isHere(params[1]) == false)
+		return user->get_server()->to_send(ERR_USERNOTINCHANNEL(getArgs(2, params[1], params[0]),
+					user->get_nick()), user->get_fd());
+
+	user->get_server()->to_send(getMsg(user, "KICK", params), chan->getFds());
+	chan->kickUser(params[1]);
+	if (chan->getUserNum() == 0)
+		user->get_server()->deleteChannel(chan->getName());
 }
