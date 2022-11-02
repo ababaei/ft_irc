d4552b9758962c5713e0bf9b9776f1d5ee894712
amontaut <alice.montaut@gmail.com>
2022-11-02 17:07:46 +0100

    coms en moins + compil stuff

--- srcs/commands/WHOIS.cpp
+++ srcs/commands/WHOIS.cpp
@@ -19,24 +19,24 @@ void    WHOIS(User *user)
 {
     std::vector<std::string> params = user->param_list;
     
-    // std::cout << "size: " << params.size() << params[0] << "\n";
-    // if (params.size() == 0)
-    //     return (user->get_server()->to_send(ERR_NONICKNAMEGIVEN(getArgs(0),user->get_nick()),
-    //     user->get_fd()));
-    // else
-    // {
-    //     User *user_info = user->get_server()->get_user(params[0]);
+    std::cout << "size: " << params.size() << params[0] << "\n";
+    if (params.size() == 0)
+        return (user->get_server()->to_send(ERR_NONICKNAMEGIVEN(getArgs(0),user->get_nick()),
+        user->get_fd()));
+    else
+    {
+        User *user_info = user->get_server()->get_user(params[0]);
         
-    //     if (user_info == NULL)
-    //         return (user->get_server()->to_send(ERR_NOSUCHNICK(getArgs(1, params[0].c_str()),
-    //         params[0]), user->get_fd()));
+        if (user_info == NULL)
+            return (user->get_server()->to_send(ERR_NOSUCHNICK(getArgs(1, params[0].c_str()),
+            params[0]), user->get_fd()));
 
-    //     if (params.size() > 1 && params[0] != user->get_server()->get_address())
-    //         return (user->get_server()->to_send(ERR_NOSUCHSERVER(getArgs(1, params[0].c_str()),
-    //         params[0]), user->get_fd()));
+        if (params.size() > 1 && params[0] != user->get_server()->get_address())
+            return (user->get_server()->to_send(ERR_NOSUCHSERVER(getArgs(1, params[0].c_str()),
+            params[0]), user->get_fd()));
 
 
-    //     return(send_infos(user, user_info->get_nick(), user_info->get_username(), user_info->get_real_name(), user_info->get_hostname(), 
-    //         user_info->get_activity(), params));
-    // }
+        return(send_infos(user, user_info->get_nick(), user_info->get_username(), user_info->get_real_name(), user_info->get_hostname(), 
+            user_info->get_activity(), params));
+    }
 }
