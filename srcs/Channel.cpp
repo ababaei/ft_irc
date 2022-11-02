88b39b4ea0c205de84875e43052830c19c5e4f95
Arthur Li <ali@desktop.home>
2022-11-02 17:01:57 +0100

    add modes topic and private

--- srcs/Channel.cpp
+++ srcs/Channel.cpp
@@ -6,7 +6,7 @@
 /*   By: amontaut <amontaut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 16:39:02 by ali               #+#    #+#             */
-/*   Updated: 2022/11/02 16:54:56 by ali              ###   ########.fr       */
+/*   Updated: 2022/11/02 17:00:42 by ali              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ Channel::Channel(const std::string &aName) : name(aName)
 	flags["moderated"] = false;
 	flags["inviteOnly"] = false;
 	flags["anonymous"] = false;
+	flags["private"] = false;
+	flags["topic"] = false;
 
 	key = "";
 	userLimit = 500;
