4e8a5e77796e986642a221021b7cdce2a370b6ad
ababaei <ababei@student.42.fr>
2022-11-09 19:07:57 +0100

    changed action on wrong PASS

--- srcs/commands/PASS.cpp
+++ srcs/commands/PASS.cpp
@@ -8,7 +8,7 @@ void    PASS(User *user)
 	if (user->get_server()->get_password() != user->param_list[0])
     {
         std::cout << RED "FAILED TO CONNECT : wrong password" E << std::endl;
-	    exit(0); //Quitter de maniere plus propre
+	    user->get_server()->close_connection (user->get_fd(), 0); //Quitter de maniere plus propre
     }
     else
     {
