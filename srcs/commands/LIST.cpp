9e593c49d5a833fd0c23a0117a90051b2f71cc89
amontaut <alice.montaut@gmail.com>
2022-11-10 17:39:31 +0100

    last rep de list a revoir maybe ?

--- srcs/commands/LIST.cpp
+++ srcs/commands/LIST.cpp
@@ -16,6 +16,20 @@
 
 //    Wildcards are allowed in the <target> parameter. --> NO car 1 server
 
+std::string itoa(int a)
+{
+    std::string ss = "";
+    while (a)
+    {
+        int x = a % 10;
+        a /= 10;
+        char i = '0';
+        i = i + x;
+        ss = i + ss;
+    }
+    return ss;
+}
+
 void LIST(User *user)
 {
     std::cout << "Hello LIST" << std::endl;
@@ -23,13 +37,25 @@ void LIST(User *user)
     //     affiche all channels (titre + topic)
     // if (list + un ou plusieurs channel en param )
     //     affiche titre + topic du/des channel
-
+    // reply : RPL_LIST :"<channel> <nombres> :<topic>"
     std::map<std::string, Channel *> channelList = user->get_server()->get_channel_list();
     if (user->param_list.size() == 0)
     {
         std::cout << YELLOW "All channels and their topic" E << std::endl;
-        for (std::map<std::string, Channel *>::iterator it = channelList.begin(); it != channelList.end(); ++it)
-            std::cout << YELLOW << it->first << "   " << it->second->getTopic() << E << std::endl;
+        for (std::map<std::string, Channel *>::iterator itm = channelList.begin(); itm != channelList.end(); ++itm)
+        {
+            int nombre = 0;
+            std::vector<std::string> listNick = itm->second->getNickList();
+            for (std::vector<std::string>::iterator it = listNick.begin(); it != listNick.end(); ++it)
+            {
+                if (user->get_server()->get_user(*it)->get_mode("invisible") == false)
+                    nombre++;
+                std::cout << ' ' << *it;
+            }
+            user->get_server()->to_send(RPL_LIST(getArgs(itm->first, itoa(nombre), itm->second->getTopic()), user->get_nick()),
+                                        user->get_fd());
+            // std::cout << YELLOW << itm->first << "   " << nombre << itm->second->getTopic() << E << std::endl;
+        }
     }
     else
     {
@@ -43,13 +69,35 @@ void LIST(User *user)
             token = s.substr(0, pos);
             if (channelList.count(token) == 1)
             {
-                std::cout << GREEN << user->get_server()->get_channel(token)->getName() << ' ' << user->get_server()->get_channel(token)->getTopic() << E << std::endl;
+                int nombre = 0;
+                std::vector<std::string> listNick = user->get_server()->get_channel(token)->getNickList();
+                for (std::vector<std::string>::iterator it = listNick.begin(); it != listNick.end(); ++it)
+                {
+                    if (user->get_server()->get_user(*it)->get_mode("invisible") == false)
+                        nombre++;
+                    std::cout << ' ' << *it;
+                }
+                user->get_server()->to_send(RPL_LIST(getArgs(user->get_server()->get_channel(token)->getName(), itoa(nombre), user->get_server()->get_channel(token)->getTopic()), user->get_nick()),
+                                            user->get_fd());
+                // std::cout << GREEN << user->get_server()->get_channel(token)->getName() << ' ' << user->get_server()->get_channel(token)->getTopic() << E << std::endl;
             }
             s.erase(0, pos + delimiter.length());
         }
         if (channelList.count(s) == 1)
         {
-                std::cout << GREEN << user->get_server()->get_channel(s)->getName() << ' ' << user->get_server()->get_channel(s)->getTopic() << E << std::endl;
+            // std::cout << GREEN << user->get_server()->get_channel(s)->getName() << ' ' << user->get_server()->get_channel(s)->getTopic() << E << std::endl;
+            int nombre = 0;
+            std::vector<std::string> listNick = user->get_server()->get_channel(s)->getNickList();
+            for (std::vector<std::string>::iterator it = listNick.begin(); it != listNick.end(); ++it)
+            {
+                if (user->get_server()->get_user(*it)->get_mode("invisible") == false)
+                    nombre++;
+                std::cout << ' ' << *it;
+            }
+            user->get_server()->to_send(RPL_LIST(getArgs(user->get_server()->get_channel(s)->getName(), itoa(nombre), user->get_server()->get_channel(s)->getTopic()), user->get_nick()),
+                                        user->get_fd());
         }
     }
+    return user->get_server()->to_send(RPL_LISTEND(getArgs(), user->get_nick()),
+                                       user->get_fd());
 }
