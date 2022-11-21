85cad3bc5b36779a49c1062c347d02b21bc6db9d
Alice MONTAUT D'ARGY <amontaut@e2r2p1.clusters.42paris.fr>
2022-11-21 15:19:21 +0100

    modif : Topic

--- srcs/commands/TOPIC.cpp
+++ srcs/commands/TOPIC.cpp
@@ -6,7 +6,7 @@
 /*   By: amontaut <amontaut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 11:42:08 by ali               #+#    #+#             */
-/*   Updated: 2022/11/21 12:29:22 by amontaut         ###   ########.fr       */
+/*   Updated: 2022/11/21 15:18:07 by amontaut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,9 @@ void TOPIC(User *user)
 		return user->getServer()->toSend(ERR_NEEDMOREPARAMS(getArgs("TOPIC"), user->getNick()),
 				user->getFd());
 	std::string channel = user->param_list[0];
-	std::string newtopic = user->param_list[1].erase(0, 1);
+	std::string newtopic = user->param_list[1];
+		if (user->param_list[1][0] == ':')
+           newtopic = user->param_list[1].substr(1, user->param_list[1].size());
 	if (newtopic.length() > 80)
 	{
 		std::cout << RED "Topic's name is too long" E << std::endl; // Pas dans rfc mais sur https://www.techbull.com/techbull/guide/internet/irccommande.html 
